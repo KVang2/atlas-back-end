9e2b213a8a1d280b78fed1fd1fb6ae8fb90bc2f2
kvang2 <kevinvaj88@hotmail.com>
2024-03-14 12:02:00 -0500

    test file

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -2,14 +2,15 @@
 """
 Returns information on TODO list progress for Employee ID
 """
-import urllib, requests
+import urllib
+import requests
 
 
 url = 'https://jsonplaceholder.typicode.com'
-TODO_list = [{"employee_id": 1, "title": "NUMBER_OF_DONE_TASKS", "content": "TOTAL_NUMBER_OF_TASKS"}]
+TODO_list = [{"employee_id": 1, "title": "NUMBER_OF_DONE_TASKS",
+              "content": "TOTAL_NUMBER_OF_TASKS"}]
 
-def employee_name(employee_id):
-    print("EMPLOYEE_NAME", employee_id)
 
-def employee_todo_list(employee_id):
+def employee_list(employee_id):
+    """ Args: employee_id (_type_):"""
     print("Employee EMPLOYEE_NAME is done with tasks(NUMBER_OF_DONE_TASKS/TOTAL_NUMBER_OF_TASKS):, employee")
