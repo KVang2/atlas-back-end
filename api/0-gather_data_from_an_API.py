589e11f0dd71c227f1ca7e680e23b55d7e824274
kvang2 <kevinvaj88@hotmail.com>
2024-03-14 13:03:55 -0500

    test codes

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -2,18 +2,16 @@
 """
 Returns information on TODO list progress for Employee ID
 """
-import urllib
 import requests
 
+url = 'https://jsonplaceholder.typicode.com/{employee_id}'
+response = requests.get(url)
+data = response.json()
 
 def employee_list(employee_id):
     """ Args: employee_id (_type_):"""
-    url = 'https://jsonplaceholder.typicode.com/{id}'
-    response = requests.get(url)
-    if response.status == 200:
-        data = response.json()
-        print(f"Employee: {data['EMPLOYEE_NAME']}, is done with tasks(
-              {data['NUMBER_OF_DONE_TASKS']}/{data['TOTAL_NUMBER_OF_TASKS']}):")
+    if response.status_code == 200:
+        print(f"Employee: {data['EMPLOYEE_NAME']}, is done with tasks({data['NUMBER_OF_DONE_TASKS']}/{data['TOTAL_NUMBER_OF_TASKS']}):")
 
 
 if __name__ == '__main__':
