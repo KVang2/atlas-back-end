45ce6b6b9e12ab240d1ca3910e540875628360b8
kvang2 <kevinvaj88@hotmail.com>
2024-03-15 15:58:40 -0500

    fix documentation

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -7,35 +7,40 @@ import sys
 import csv
 
 
-args = sys.argv
-user_id = args[1]
-url = 'https://jsonplaceholder.typicode.com/'
+class Get_Todo():
+    """ Class for employee info"""
 
-user_result = requests.get(url + "users/" + user_id)
-todos_result = requests.get(url + "todos")
-user_json = user_result.json()
-todos_json = todos_result.json()
+    def employee_list(self):
+        """ To grab employee info"""
+        args = sys.argv
+        user_id = args[1]
+        url = 'https://jsonplaceholder.typicode.com/'
 
-EMPLOYEE_NAME = user_json["username"]
-user_list = [task for task in todos_json
+        user_result = requests.get(url + "users/" + user_id)
+        todos_result = requests.get(url + "todos")
+        user_json = user_result.json()
+        todos_json = todos_result.json()
+
+        EMPLOYEE_NAME = user_json["username"]
+        user_list = [task for task in todos_json
              if task["userId"] == user_id]
 
-fields = ['USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS', 'TASK_TITLE']
-# writing to csv file
-csv_file_path = f'{user_id}.csv'
-with open(csv_file_path, 'w', newline='') as csvfile:
-    writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-    writer.writerow(fields)
+        fields = ['USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS', 'TASK_TITLE']
+        # writing to csv file
+        csv_file_path = f'{user_id}.csv'
+        with open(csv_file_path, 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+            writer.writerow(fields)
 
-    # writing data
-    for task in user_list:
-        if str(task['userId']) == user_id:
-            completed_status = "True" if task["completed"] else "False"
-            writer.writerow([task["userId"],
-                EMPLOYEE_NAME, completed_status, 
-                task["title"]])
+            # writing data
+            for task in user_list:
+                if str(task['userId']) == user_id:
+                    completed_status = "True" if task["completed"] else "False"
+                    writer.writerow([task["userId"],
+                        EMPLOYEE_NAME, completed_status, 
+                        task["title"]])
 
-    print(f"Data exported to {csv_file_path}")
+            print(f"Data exported to {csv_file_path}")
 
 if __name__ == "__main__":
     Get_Todo().employee_list()
